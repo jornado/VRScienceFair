7c56f821447ba22f88b04c3066692ebb8fcdd6e9
Jen Oslislo <poeksweb@gmail.com>
2017-08-05 08:05:00 -0700

    Materials, Video, Hallway scene

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    public void ChangeToMain()
+    {
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(3);
+        Application.LoadLevel("main");
+    }
+
+}
