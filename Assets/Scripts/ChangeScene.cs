40e1560c98b8266c36cad17572d3b24b51b66717
Jen Oslislo <poeksweb@gmail.com>
2017-08-07 10:09:07 -0700

    Some intro scene models and screenshots

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class ChangeScene : MonoBehaviour
 {
+    public Texture fadeImage;
+    public float waitTime = 3f;
+
+    private float fadeSpeed = .125f;
+    private int drawDepth = -1000;    
+    private float alpha = 0.0f;
+    private int fadeDir = -1;
+    private bool isFadingOut = false;
 
     public void ChangeToMain()
     {
@@ -12,8 +20,22 @@ public class ChangeScene : MonoBehaviour
 
     IEnumerator Wait()
     {
-        yield return new WaitForSeconds(3);
+        isFadingOut = true;
+        yield return new WaitForSeconds(waitTime);
         Application.LoadLevel("main");
     }
-
+    
+    void OnGUI()
+    {
+        if (isFadingOut)
+        {
+            alpha -= fadeDir * fadeSpeed * Time.deltaTime;
+            alpha = Mathf.Clamp01(alpha);
+            Color thisAlpha = GUI.color;
+            thisAlpha.a = alpha;
+            GUI.color = thisAlpha;
+            GUI.depth = drawDepth;
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeImage);
+        }
+    }
 }
--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # VRScienceFair
 
-* Google Cardboard VR research project for Udacity VR Nanodegree. Explores different locomotion mechanics in a fun science fair environment.
+* VR Science Fair is a Google Cardboard VR research project for my Udacity VR Nanodegree. It explores different locomotion mechanics in a fun science fair environment.
 
 ## Requirements
 * Unity 5.5+
-* Google VR
+* Google VR (version is same as that in Puzzler)
 * iTween
 * Android Studio + JDK or XCode and all that provisioning jazz. (to build to device, if desired) 
 * Google Cardboard (if building to device)
+
+
